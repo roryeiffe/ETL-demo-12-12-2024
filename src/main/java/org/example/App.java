11d239319e9a19e35e341369a0eeb36444f5b263
RoryEiffe <roryeiffe8@gmail.com>
2024-12-12 13:19:18 -0500

    Person Model Class
    Added a Person class to hold the data that we get from the external file source. Also, added the file source itself (csv file)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.models.Person;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person person = new Person(1, "Bob", 20, "NYC");
+        System.out.println(person);
     }
 }
