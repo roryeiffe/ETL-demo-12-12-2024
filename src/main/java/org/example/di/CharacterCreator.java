85900162a3a2d56d16e63384c512d5ad47c41f19
RoryEiffe <roryeiffe8@gmail.com>
2024-12-19 13:32:12 -0500

    Implemented a Player Creator
    Including a scanner to read input from the console and then creating a player object accordingly, injecting the proper Tool.

--- src/main/java/org/example/di/CharacterCreator.java
+++ src/main/java/org/example/di/CharacterCreator.java
@@ -1,9 +1,36 @@
 package org.example.di;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CharacterCreator {
     public static void main(String[] args) {
+        // set up a Scanner object to retrieve user input from System.in(console)
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name => ");
+        String name = scanner.nextLine();
 
+        System.out.println("You entered: " + name);
+
+        System.out.print("Enter your specialization => ");
+        String specialization = scanner.nextLine();
+
+        System.out.println("You entered: " + specialization);
+
+        Tool tool;
+        if(specialization.toLowerCase(Locale.ROOT).equals("knight")) {
+            tool = new Sword();
+        }
+        else if(specialization.toLowerCase(Locale.ROOT).equals("acrobat")) {
+            tool = new Grappler();
+        }
+        else {
+            tool = new WoodenSword();
+        }
+
+        Player player = new Player(name, specialization, tool);
+        System.out.println(player);
 
     }
 }
---
+++ src/main/java/org/example/di/WoodenSword.java
@@ -0,0 +1,7 @@
+package org.example.di;
+
+public class WoodenSword implements Tool{
+    public void use() {
+        System.out.println("Swinging wooden sword!");
+    }
+}
